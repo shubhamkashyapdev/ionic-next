fec9a3c13746813760109b27148fa1588e0aacb6
shubhamwebdesign <hex.media.1800@gmail.com>
2023-06-03 00:29:25 +0530

    feat: capacitor setup done

---
+++ android/app/src/main/java/com/ionicreact/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.ionicreact.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,12 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli"
 
 const config: CapacitorConfig = {
-  appId: 'com.ionicreact.app',
-  appName: 'ionicreact',
-  webDir: 'public',
+  appId: "com.ionicreact.app",
+  appName: "ionicreact",
+  webDir: "out",
   server: {
-    androidScheme: 'https'
-  }
-};
+    androidScheme: "https",
+  },
+}
 
-export default config;
+export default config
